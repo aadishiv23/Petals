b5cc52eb4d05f03d08f94a7ee741f144dd02bfd0
aadishiv <aadishiv@outlook.com>
2025-03-12 14:34:06 -0400

    Create GeminiCompatibleTool protocol

--- PetalTools/Sources/PetalTools/Protocols/GeminiCompatibleTool.swift
+++ PetalTools/Sources/PetalTools/Protocols/GeminiCompatibleTool.swift
@@ -1,8 +1,15 @@
 //
-//  File.swift
+//  GeminiCompatibleTool.swift
 //  PetalTools
 //
 //  Created by Aadi Shiv Malhotra on 3/11/25.
 //
 
 import Foundation
+import GoogleGenerativeAI
+
+/// Protocol for tools that can be used with Gemini.
+public protocol GeminiCompatibleTool: PetalTool {
+    /// Converts this tool into a Gemini function declaration.
+    func asGeminiFunctionDeclaration() -> FunctionDeclaration
+}
