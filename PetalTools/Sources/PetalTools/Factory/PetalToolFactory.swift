5c073d689e6cb56d837ce21aabf34c540c81d87f
aadishiv <aadishiv@outlook.com>
2025-03-12 14:31:57 -0400

    Create PetalToolFactory

--- PetalTools/Sources/PetalTools/Factory/PetalToolFactory.swift
+++ PetalTools/Sources/PetalTools/Factory/PetalToolFactory.swift
@@ -1,8 +1,26 @@
 //
-//  File.swift
+//  PetalToolFactory.swift
 //  PetalTools
 //
 //  Created by Aadi Shiv Malhotra on 3/11/25.
 //
 
 import Foundation
+
+/// Factory for creating standardized tools.
+public class PetalToolFactory {
+    
+    /// Creates a generic calendar tool.
+    public static func createCalendarTool() async -> any PetalTool {
+        let calendarTool = PetalMockCalendarTool()
+        await PetalToolRegistry.shared.registerTool(calendarTool)
+        return calendarTool
+    }
+    
+    /// Create a generic canvas tool.
+    public static func createFetchCanvasCoursesTool() async -> any PetalTool {
+        let canvasTool = PetalGenericFetchCanvasCoursesTool()
+        await PetalToolRegistry.shared.registerTool(canvasTool)
+        return canvasTool
+    }
+}
