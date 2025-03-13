fde9ead970bab0298202d653e2207786a0e47583
aadishiv <aadishiv@outlook.com>
2025-03-12 20:58:52 -0400

    WIP - Add more canvas models

--- PetalTools/Sources/PetalTools/Tools/Models/CanvasModels.swift
+++ PetalTools/Sources/PetalTools/Tools/Models/CanvasModels.swift
@@ -23,3 +23,29 @@ public struct CanvasCourse: Decodable {
         case completedAt = "completed_at"
     }
 }
+
+public struct CanvasAssignment: Decodable {
+    public let id: Int
+    public let name: String
+    public let dueAt: String?
+
+    public enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case dueAt = "due_at"
+    }
+}
+
+public struct CanvasSubmission: Decodable {
+    public let assignmentId: Int
+    public let grade: String?
+
+    public enum CodingKeys: String, CodingKey {
+        case assignmentId = "assignment_id"
+        case grade
+    }
+
+    public var assignmentName: String {
+        "Assignment \(assignmentId)"
+    }
+}
