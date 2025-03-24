f2b4e08241a60ef7cac3b60de870f57c4d41e8f2
aadishiv <aadishiv@outlook.com>
2025-03-24 10:37:11 -0400

    add device stat and add ios target real

--- Petals/Utilities/DeviceStat.swift
+++ Petals/Utilities/DeviceStat.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import MLXLLM
+import MLX
+import Observation
+
+@Observable
+final class DeviceStat: @unchecked Sendable {
+    
+    /// The variable holding a snapshot of GPU memory state.
+    @MainActor
+    var gpuUsage = GPU.snapshot()
+    
+    private let initialGPUSnapshot = GPU.snapshot()
+    
+    private var timer: Timer?
+    
+    init() {
+        Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { [weak self] _ in
+            self?.updateGPUUsages()
+        }
+    }
+    
+    deinit {
+        timer?.invalidate()
+    }
+    
+    private func updateGPUUsages() {
+        let gpuSnapshotDelta = initialGPUSnapshot.delta(GPU.snapshot())
+        DispatchQueue.main.async { [weak self] in
+            self?.gpuUsage = gpuSnapshotDelta
+        }
+    }
+}
+
